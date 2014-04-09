4cc81dd9b3d4ba4436d464aeadb1c7efc5d416b1
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-04-09 13:53:45 +0200

    Added a basic template so that I got something to work with. Changed around where the models are placed so that I do not need to have them in the same file as the application.

--- ApplicationModel.py
+++ ApplicationModel.py
@@ -1,25 +1,22 @@
 __author__ = 'Chrille'
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
+#from flask import Flask
+import flask.ext.sqlalchemy
+#from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+#Base = declarative_base()
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bubblegum123@localhost/postgres'
-db = SQLAlchemy(app)
-
-"""ug_has_v_table = db.Table('association', Base.metadata,
-                          db.Column('left_id', db.Integer, db.ForeignKey('user_group.ugid'),primary_key=True),
-                          db.Column('right_id', db.Integer, db.ForeignKey('Visibility.vid'),primary_key=True))"""
+#app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bubblegum123@localhost/postgres'
+#db = SQLAlchemy(app)
+db = flask.ext.sqlalchemy.SQLAlchemy()
 
 class User(db.Model):
     userid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120),unique=True)
+    username = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(256))
-    postscreated = db.Column(db.Integer,default=0)
-    comments = db.Column(db.Integer,default=0)
+    postscreated = db.Column(db.Integer, default=0)
+    comments = db.Column(db.Integer, default=0)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -31,6 +28,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.userid'))
@@ -38,7 +36,7 @@ class Comment(db.Model):
     commentid = db.Column(db.Integer, db.ForeignKey('comment.id'), nullable=True)
     content = db.Column(db.String(2000))
 
-    def __init__(self, userid, postid, content, commentid = None):
+    def __init__(self, userid, postid, content, commentid=None):
         self.userid = userid
         self.postid = postid
         self.content = content
@@ -47,7 +45,8 @@ class Comment(db.Model):
     def __repr__(self):
         return '<Comment {}>'.format(self.content[:15])
 
-class User_Group(db.Model):
+
+class UserGroup(db.Model):
     __tablename__ = 'usergroup'
     ugid = db.Column(db.Integer, primary_key=True)
     ugname = db.Column(db.String(50), unique=True)
@@ -59,6 +58,7 @@ class User_Group(db.Model):
     def __repr__(self):
         return '<User_Group {}>'.format(self.ugname)
 
+
 class Category(db.Model):
     categoryid = db.Column(db.Integer, primary_key=True)
     categoryname = db.Column(db.String(100), unique=True)
@@ -69,25 +69,27 @@ class Category(db.Model):
     def __repr__(self):
         return '<Category {}>'.format(self.categoryname)
 
+
 class Group(db.Model):
     groupid = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('user.userid'),nullable=False)
-    title = db.Column(db.String(250),nullable=False)
+    userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=False)
+    title = db.Column(db.String(250), nullable=False)
 
     def __init__(self, userid, title):
         self.userid = userid
         self.title = title
 
     def __repr__(self):
-        return '<Group {} created by {}>'.format(self.title,self.userid)
+        return '<Group {} created by {}>'.format(self.title, self.userid)
+
 
 class Post(db.Model):
     postid = db.Column(db.Integer, primary_key=True)
-    createdby = db.Column(db.Integer, db.ForeignKey('user.userid'),nullable=False)
+    createdby = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=False)
     timeposted = db.Column(db.DateTime, nullable=False)
     views = db.Column(db.Integer, default=0)
-    content = db.Column(db.String(2000),nullable=False)
-    typeid = db.Column(db.Integer, db.ForeignKey('type.typeid'),nullable=False)
+    content = db.Column(db.String(2000), nullable=False)
+    typeid = db.Column(db.Integer, db.ForeignKey('type.typeid'), nullable=False)
     title = db.Column(db.String(250), nullable=False)
 
     def __init__(self, createdby, timeposted, content, typeid, title):
@@ -100,6 +102,7 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.title)
 
+
 class Type(db.Model):
     typeid = db.Column(db.Integer, primary_key=True)
     typename = db.Column(db.String(50), unique=True)
@@ -109,6 +112,7 @@ class Type(db.Model):
 
     def __repr__(self):
         return '<Type {}>'.format(self.typename)
+
 
 class Visibility(db.Model):
     vid = db.Column(db.Integer, primary_key=True)
@@ -120,13 +124,14 @@ class Visibility(db.Model):
     def __repr__(self):
         return '<Visibility {}>'.format(self.vname)
 
+
 class UG_has_V(db.Model):
     __tablename__ = 'ughasv'
     ugid = db.Column(db.Integer, db.ForeignKey('usergroup.ugid'))
     vid = db.Column(db.Integer, db.ForeignKey('visibility.vid'))
 
     __table_args__ = (
-        db.PrimaryKeyConstraint('ugid','vid'),
+        db.PrimaryKeyConstraint('ugid', 'vid'),
         {},
     )
 
