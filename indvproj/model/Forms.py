9d392f0bf2e2ddba060eedf571639f44f107ecc1
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-04-23 10:08:34 +0200

    Changed .gitignore to not include the pycharm .idea directory

--- indvproj/model/Forms.py
+++ indvproj/model/Forms.py
@@ -1,23 +1,29 @@
 from flask_wtf import Form
-from wtforms import BooleanField, TextField, PasswordField, validators, TextAreaField
+from wtforms import TextField, PasswordField, validators, TextAreaField
+
 __author__ = 'Chrille'
+
 
 class RegistrationForm(Form):
     username = TextField("Username", [validators.Length(min=5, max=120)])
-    email = TextField("Email", [validators.Length(min=6, max=120)])
+    email = TextField("Email", [validators.Length(min=6)])
     password = PasswordField("New password", [
         validators.Required(),
-        validators.EqualTo("confirm", message="Passwords must match")
+        validators.EqualTo("confirm", message="Passwords must match"),
+        validators.Length(min=6)
     ])
     confirm = PasswordField("Repeat password")
 
+
 class NewPostForm(Form):
-    title = TextField("Title",[validators.Length(min=10, max=250),validators.Required()])
-    content =  TextAreaField("Content here", [validators.Length(min=10, max=2000),validators.Required()])
+    title = TextField("Title", [validators.Length(min=10, max=250), validators.Required()])
+    content = TextAreaField("Content here", [validators.Length(min=10, max=2000), validators.Required()])
+
 
 class LoginForm(Form):
-    username = TextField("Username",[validators.Length(min=3,max=120),validators.Required()])
-    password = PasswordField("Password",[validators.Length(min=6),validators.Required()])
+    username = TextField("Username", [validators.Length(min=3, max=120), validators.Required()])
+    password = PasswordField("Password", [validators.Length(min=6), validators.Required()])
+
 
 class CollectionForm(Form):
     title = TextField("Title", [validators.Length(min=10, max=250)])
