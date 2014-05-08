ae9b7e90088394a2824f41efc87d0fec69502a59
Christfoffer Holmgren <ch222kv@student.lnu.se>
2014-05-08 10:11:54 +0200

    * Added docstrings to all views

--- indvproj/model/views.py
+++ indvproj/model/views.py
@@ -25,13 +25,13 @@ def getsalt(length):
     return os.urandom(length)
 
 
-def escape_text_and_create_markdown(unescaped_text):
+def escape_text_and_create_markdown(unescaped_text, safe_mode="escape"):
     """
     Takes unsafe input and escapes it and converts it to safe html using markdown
     Safe_mode set to escapes doesn't remove anything and allows the users to post what ever they want without it getting
     removed.
     """
-    return markdown(unescaped_text, safe_mode="escape")
+    return markdown(unescaped_text, safe_mode=safe_mode)
 
 
 def encrypt(password):
@@ -63,6 +63,10 @@ def check_password(string_password, salt):
 
 
 class MainView(FlaskView):
+    """
+    MainView handles ourpage.tld/
+    I.e: takes care of the frontpage
+    """
     route_base = '/'
 
     def index(self):
@@ -76,18 +80,42 @@ class MainView(FlaskView):
         #print(current_user)
         #print(session)
         #print(dir(session))
+        """
+        Index is the mainpage of the site.
+        You redirect here if you want to get back to the start of the site.
+
+        :return: rendered template main.html with the options inserted
+        """
         return render_template('main.html',
                                posts=Post.query.all(), categories=Category.query.all(), users=User.query.all())
 
 
 class BlogView(FlaskView):
+    """
+    Shorthand for /c/blog
+    """
+
     def index(self):
+        """
+        Takes care of /blog/ and returns CategoryView.get('blog')
+        """
         return CategoryView.get(self, 'blog')
 
 
 class LoginView(FlaskView):
+    """
+    The LoginView takes care of login via Flask_login
+    Does the error checking and the password checking
+    """
+
     @route('/', methods=['GET', 'POST'])
     def index(self):
+        """
+        This is the login page and it handels evertyhing around login.
+        Checks if the user exists and logs it in, otherwise it handles the other cases.
+
+        :return: Either a redirect to MainView, LoginView:index again or a rendered template of login.html
+        """
         if current_user.is_active():
             return redirect(url_for('MainView:index'))
 
@@ -107,6 +135,10 @@ class LoginView(FlaskView):
 
 
 class LogoutView(FlaskView):
+    """
+    LOgs the user out via Flask_login
+    """
+
     @login_required
     def logout(self):
         logout_user()
@@ -115,10 +147,20 @@ class LogoutView(FlaskView):
 
 
 class PostView(FlaskView):
+    """
+    PostView handles everything that is centered around specific posts such as deletion and commenting,
+    and also creation of new posts
+    """
     route_base = '/p'
 
     def get(self, postid):
-        #return "Hello from PostView:get"
+        """
+        Redirects to CategoryView:view_post if the post has a category (exists)
+        Else it renders template post.html
+        :param postid: The postid of the post to view
+        :return: Either a redirect to CategoryView:view_post if the post exists, or a rendered template of post.html
+        """
+        # TODO: Change this to a missing_post template instead
         post = Post.query.get(postid)
         print(dir(Comment.query.filter(Comment.commentid in Post.comments)))
         form = DeletePostForm()
@@ -128,9 +170,18 @@ class PostView(FlaskView):
                                     categoryname=Category.query.get(post.categoryid).categoryname))
         return render_template('post.html', post=Post.query.get(postid), form=form)
 
+    # TODO: Add more methods for deletion that isn't permanent, set a status or something
     @route('/<postid>/delete', methods=['POST'])
     @login_required
     def delete(self, postid):
+        """
+        Deletes the post associated with the postid, but only if the user is the created or a moderator of the category
+        This is a permanent deletion, it won't be marked invisible or something, it will get deleted
+        Use with caution
+
+        :param postid: Postid of post to delete
+        :return: Redirects to MainView:index all the time
+        """
         try:
             post = Post.query.get(postid)
             category = Category.query.get(post.categoryid)
@@ -150,6 +201,13 @@ class PostView(FlaskView):
     @route('/<postid>/comment', methods=['POST'])
     @login_required
     def comment(self, postid):
+        """
+        Handles commenting on a post. At this time it's only possible to have top-level comments,
+        but that should change in the future to include child comments
+
+        :param postid: Postid to comment on
+        :return:
+        """
         print("Inside of comment")
         form = CommentForm()
         if form.validate_on_submit():
@@ -168,6 +226,14 @@ class PostView(FlaskView):
     @route('/new/', methods=['GET', 'POST'])
     @login_required
     def new_post(self, categoryname=""):
+        """
+        Allow you to create new posts in a category. The inputbox with categoryname will have default value\
+         of categoryname
+
+        :param categoryname: A potential categoryname to be set in the form, which can be used when you want to display\
+        that you are in [categoryname] and the box is already inputted
+        :return:
+        """
         print("We are inside new_post of PostView")
         form = TextPostForm()
         if not form.categoryname.data:
@@ -200,12 +266,24 @@ class PostView(FlaskView):
 
 
 class RegisterView(FlaskView):
+    """
+    Handles Registration of users
+    """
+
     """def index(self):
         form = RegistrationForm(request.form)
         return render_template('create_user.html', form=form)"""
 
     @route('/', methods=['GET', 'POST'])
     def new_user(self):
+        """
+        Allows creation of users
+        Checks that no user has the same username and also that no one has the same email
+        Then it tries to create the user
+
+        :return: Either a redirect to MainView:index if the user is already registered and logged in,
+        a redirect to RegisterView:new_user again or a rendered template of create_user.html
+        """
         if current_user.is_active():
             return redirect(url_for('MainView:index'))
 
@@ -242,9 +320,19 @@ class RegisterView(FlaskView):
 
 
 class UserView(FlaskView):
+    """
+    Handles Userpages found under /u
+    """
     route_base = '/u'
 
     def get(self, username):
+        """
+        Handles /u/<username>
+        Since usernames are unique it allows us to have that route
+
+        :param username: The username of the user to lookup
+        :return: Either a rendered template of user.html or user_missing.html if there isn't a user with that username
+        """
         user = User.query.filter_by(username=username).first()
         if user is not None:
             print(user.collections.all())
@@ -253,15 +341,29 @@ class UserView(FlaskView):
 
 
 class CategoryView(FlaskView):
+    """
+    CategoryView handles everything centered around categories such as new categories, moderators
+    """
     route_base = '/c'
 
     def index(self):
+        """
+        Displays all categories
+
+        :return:
+        """
         categories = Category.query.all()
         print(categories)
         return render_template("all_categories.html", categories=categories)
 
     @route('/<categoryname>/')
     def get(self, categoryname):
+        """
+        Displays all posts in a category
+
+        :param categoryname:
+        :return:
+        """
         from sqlalchemy import func
 
         category = Category.query.filter(func.lower(Category.categoryname) == func.lower(categoryname)).first()
@@ -272,6 +374,15 @@ class CategoryView(FlaskView):
 
     @route('<categoryname>/p/<postid>')
     def view_post(self, categoryname, postid):
+        """
+        Displays a post in a category
+        Does some checking on the user and such t oeither render deletion buttons or not, but it can
+        probably be simplified
+
+        :param categoryname:
+        :param postid:
+        :return:
+        """
         from sqlalchemy import func
 
         form = DeletePostForm()
@@ -294,6 +405,12 @@ class CategoryView(FlaskView):
     @route('<categoryname>/p/new', methods=['GET', 'POST'])
     @login_required
     def new_post(self, categoryname):
+        """
+        Returns PostView.new_post since PostView handles all creation of posts
+
+        :param categoryname:
+        :return:
+        """
         return PostView.new_post(self, categoryname)
         """
         print(request.method)
@@ -328,6 +445,11 @@ class CategoryView(FlaskView):
     @route('/new', methods=['GET', 'POST'])
     @login_required
     def new_category(self):
+        """
+        Creates a new category as long as categoryname is unique
+
+        :return:
+        """
         print('We are inside CategoryView:new_category')
         form = CategoryForm()
         if form.validate_on_submit():
@@ -353,6 +475,12 @@ class CategoryView(FlaskView):
 
     @route('/<categoryname>/moderators/')
     def moderators(self, categoryname):
+        """
+        Displays all moderators in a category
+
+        :param categoryname: Categoryname to display moderators in
+        :return:
+        """
         return render_template('moderators.html', category=Category.query.filter_by(categoryname=categoryname).first())
 
     @route('<categoryname>/moderators/add', methods=['GET', 'POST'])
@@ -360,6 +488,7 @@ class CategoryView(FlaskView):
     def add_moderator(self, categoryname):
         """
         Add moderators to a category
+
         :param categoryname:
         :return:
         """
@@ -379,14 +508,32 @@ class CategoryView(FlaskView):
 
 
 class CollectionView(FlaskView):
+    """
+    CollectionView handles everything around collections
+    New collections, adding links and such
+    Collections are private and as such you will only be able to view your own, unless you got a password, I think
+    """
+
     @login_required
     def index(self):
+        """
+        Displays all collection of the logged in user
+
+        :return: Rendered template of collections.html which displays all collections of the user
+        """
         return render_template('collections.html', title="Displays your collections",
                                collections=current_user.collections)
 
     @route('/<collectionid>/delete', methods=['POST'])
     @login_required
     def delete(self, collectionid):
+        """
+        Deletes the collection, since we use csrf_protection the user cannot remove a collection that they
+        aren't allowed to delete, so we don't need any checking of the user
+
+        :param collectionid: Collection to delete
+        :return:
+        """
         try:
             collection = Collection.query.get(collectionid)
             db_session.delete(collection)
@@ -401,6 +548,12 @@ class CollectionView(FlaskView):
     @route('/<collectionid>/add_link', methods=['POST'])
     @login_required
     def add_link(self, collectionid):
+        """
+        Adds a link to the collection if the user is alloweed to do so
+
+        :param collectionid: Id of collection to add the link to
+        :return:
+        """
         addform = AddToCollectionForm()
         if addform.validate_on_submit():
             oldpost = Post.query.get(addform.link.data)
@@ -423,6 +576,13 @@ class CollectionView(FlaskView):
 
     @login_required
     def get(self, id):
+        """
+        Gets the collection with the given id, if the user is allowed to view that collection,
+        i.e. if he created it
+
+        :param id: Collectionid to lookup
+        :return:
+        """
         collection = Collection.query.get(id)
         print(dir(collection))
         print(collection.links.all())
@@ -441,6 +601,12 @@ class CollectionView(FlaskView):
     @route('/new', methods=['GET', 'POST'])
     @login_required
     def new_collection(self):
+        """
+        Creates a new collection
+
+        :return: Either a redirect to CollectionView:get with the created collection, a redirect to the same view or
+        rendered template of create_collection.html
+        """
         form = CollectionForm()
         if form.validate_on_submit():
             try:
