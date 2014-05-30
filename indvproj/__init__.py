f75d86a4a34f9e3d4b3767a4cae3ee3fae054faf
Christfoffer Holmgren <ch222kv@student.lnu.se>
2014-05-30 10:42:57 +0200

    Yes

--- indvproj/__init__.py
+++ indvproj/__init__.py
@@ -6,8 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "Wtf is wrong with you? Why won't you just let me register a user sometime today? :("
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:thisisapassword@localhost/postgres'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:bubblegum123@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:thisisapassword@localhost/postgres'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:bubblegum123@localhost/postgres'
 
 db = SQLAlchemy(app)
 db_session = db.session
